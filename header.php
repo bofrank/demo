0eab6541c48206fb39cb906bd5b6ec65b83c1aa4
bofrank <bofrank@Bolinass-MacBook-Air.local>
2014-12-04 15:22:11 -0800

    chat display with phone number

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
 <div data-role="header" data-id="data-header" data-position="fixed">
-	<h1><a href="#page_00" style="text-decoration:none;" id="link_00">TopicB v.4</a></h1>
+	<h1><a href="#page_00" style="text-decoration:none;" id="link_00">TopicB</a></h1>
 </div>
--- index.php
+++ index.php
@@ -11,70 +11,84 @@
 
 <script>
 
-		$(document).ready(function() {
+	$(document).ready(function() {
 
-			//show button as active in navigation
-			$(document).on("pageshow", '#page_01', function () {
-		    $('#page_01').find("#link_01").addClass("ui-btn-active ui-state-persist");
-		  }).on("pageshow", '#page_02', function () {
-		    $('#page_02').find("#link_02").addClass("ui-btn-active ui-state-persist");
-		  }).on("pageshow", '#page_03', function () {
-		    $('#page_03').find("#link_03").addClass("ui-btn-active ui-state-persist");
-		  }).on("pageshow", '#page_04', function () {
-		    $('#page_04').find("#link_04").addClass("ui-btn-active ui-state-persist");
-		  }).on("pageshow", '#page_05', function () {
-		    $('#page_05').find("#link_05").addClass("ui-btn-active ui-state-persist");
-		  });
+		//show button as active in navigation
+		$(document).on("pageshow", '#page_01', function () {
+	    $('#page_01').find("#link_01").addClass("ui-btn-active ui-state-persist");
+	  }).on("pageshow", '#page_02', function () {
+	    $('#page_02').find("#link_02").addClass("ui-btn-active ui-state-persist");
+	  }).on("pageshow", '#page_03', function () {
+	    $('#page_03').find("#link_03").addClass("ui-btn-active ui-state-persist");
+	  }).on("pageshow", '#page_04', function () {
+	    $('#page_04').find("#link_04").addClass("ui-btn-active ui-state-persist");
+	  }).on("pageshow", '#page_05', function () {
+	    $('#page_05').find("#link_05").addClass("ui-btn-active ui-state-persist");
+	  });
 
-			//handle swipe left
-			$(document).on("swipeleft", '#page_00', function () {
-				$('#link_01').removeAttr("data-direction");
-		    $('#link_01').trigger('click');
-		  }).on("swipeleft", '#page_01', function () {
-		  	$('#link_02').removeAttr("data-direction");
-		    $('#link_02').trigger('click');
-		  }).on("swipeleft", '#page_02', function () {
-		  	$('#link_03').removeAttr("data-direction");
-		    $('#link_03').trigger('click');
-		  }).on("swipeleft", '#page_03', function () {
-		  	$('#link_04').removeAttr("data-direction");
-		    $('#link_04').trigger('click');
-		  }).on("swipeleft", '#page_04', function () {
-		  	$('#link_05').removeAttr("data-direction");
-		    $('#link_05').trigger('click');
-		  }).on("swipeleft", '#page_05', function () {
-		  	$('#link_00').removeAttr("data-direction");
-		    $('#link_00').trigger('click');
-		  });
+		//handle swipe left
+		$(document).on("swipeleft", '#page_00', function () {
+			$('#link_01').removeAttr("data-direction");
+	    $('#link_01').trigger('click');
+	  }).on("swipeleft", '#page_01', function () {
+	  	$('#link_02').removeAttr("data-direction");
+	    $('#link_02').trigger('click');
+	  }).on("swipeleft", '#page_02', function () {
+	  	$('#link_03').removeAttr("data-direction");
+	    $('#link_03').trigger('click');
+	  }).on("swipeleft", '#page_03', function () {
+	  	$('#link_04').removeAttr("data-direction");
+	    $('#link_04').trigger('click');
+	  }).on("swipeleft", '#page_04', function () {
+	  	$('#link_05').removeAttr("data-direction");
+	    $('#link_05').trigger('click');
+	  }).on("swipeleft", '#page_05', function () {
+	  	$('#link_00').removeAttr("data-direction");
+	    $('#link_00').trigger('click');
+	  });
 
-		  //handle swipe right
-		  $(document).on("swiperight", '#page_01', function () {
-		    $('#link_00').trigger('click');
-		    $('#link_00').attr("data-direction","reverse");
-		  }).on("swiperight", '#page_02', function () {
-		  	$('#link_01').attr("data-direction","reverse");
-		    $('#link_01').trigger('click');
-		  }).on("swiperight", '#page_03', function () {
-		  	$('#link_02').attr("data-direction","reverse");
-		    $('#link_02').trigger('click');
-		  }).on("swiperight", '#page_04', function () {
-		  	$('#link_03').attr("data-direction","reverse");
-		    $('#link_03').trigger('click');
-		  }).on("swiperight", '#page_05', function () {
-		  	$('#link_04').attr("data-direction","reverse");
-		    $('#link_04').trigger('click');
-		  }).on("swiperight", '#page_00', function () {
-		  	$('#link_05').attr("data-direction","reverse");
-		    $('#link_05').trigger('click');
-		  });
+	  //handle swipe right
+	  $(document).on("swiperight", '#page_01', function () {
+	    $('#link_00').trigger('click');
+	    $('#link_00').attr("data-direction","reverse");
+	  }).on("swiperight", '#page_02', function () {
+	  	$('#link_01').attr("data-direction","reverse");
+	    $('#link_01').trigger('click');
+	  }).on("swiperight", '#page_03', function () {
+	  	$('#link_02').attr("data-direction","reverse");
+	    $('#link_02').trigger('click');
+	  }).on("swiperight", '#page_04', function () {
+	  	$('#link_03').attr("data-direction","reverse");
+	    $('#link_03').trigger('click');
+	  }).on("swiperight", '#page_05', function () {
+	  	$('#link_04').attr("data-direction","reverse");
+	    $('#link_04').trigger('click');
+	  }).on("swiperight", '#page_00', function () {
+	  	$('#link_05').attr("data-direction","reverse");
+	    $('#link_05').trigger('click');
+	  });
 
+	  $("#topic_list a").click(function(){
+		  chatStart($(this).text());
 		});
 
-		var topic="default";
-		function chatStart(topic){
-			$.mobile.changePage("#page_03");
-			$("#chatBox").attr("src","../index_chat.php?chatter=0000000000&chatee=1111111111&topicinit="+topic);
-		}
+		
+
+	});
+
+	//create unique id (this will eventually become tapid from sip server)
+	var cookie = "";
+  var possible = "0123456789";
+
+  for( var i=0; i < 10; i++ )
+      cookie += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	//create chat from clicking on topic
+	var topic="default";
+	function chatStart(topic){
+		$.mobile.changePage("#page_03");
+		$("#chatBox").attr("src","../index_chat.php?chatter="+cookie+"&chatee=1111111111&topicinit="+topic);
+	}
 
 </script>
 
@@ -85,14 +99,17 @@
 	<?php include('header.php'); ?>
 
 	<div role="main" class="ui-content">
-    <ul data-role="listview" data-filter="true" data-filter-placeholder="Search TopicB" data-inset="true" data-filter-reveal="true">
-				<li><a onclick="chatStart($(this).text());">Heli-Skiing</a></li>
-				<li><a onclick="chatStart($(this).text());">Travel Agent</a></li>
-				<li><a onclick="chatStart($(this).text());">Snow Report</a></li>
-				<li><a onclick="chatStart($(this).text());">Lodging</a></li>
-				<li><a onclick="chatStart($(this).text());">Kids Snow School</a></li>
-				<li><a onclick="chatStart($(this).text());">Restaurants</a></li>
-				<li><a onclick="chatStart($(this).text());">Alpine Tour Bindings</a></li>
+    <ul data-role="listview" data-filter="true" data-filter-placeholder="Search TopicB" data-inset="true" data-filter-reveal="true" id="topic_list">
+				<li><a>Heli-Skiing</a></li>
+				<li><a>Travel Agent</a></li>
+				<li><a>Snow Report</a></li>
+				<li><a>Lodging</a></li>
+				<li><a>Kids Snow School</a></li>
+				<li><a>Restaurants</a></li>
+				<li><a>Alpine Tour Bindings</a></li>
+				<li><a>fresh powder</a></li>
+				<li><a>best month for powder</a></li>
+				<li><a>powder skiis</a></li>
 		</ul>
 	</div>
 
@@ -131,9 +148,9 @@
 
 	<div role="main" class="ui-content">
 
-		<div style="width:300px;margin-left:auto;margin-right:auto;padding-left:15px;padding-right:15px;background:#999;">
+		<div style="width:90%;margin-left:auto;margin-right:auto;padding-left:15px;padding-right:15px;background:#999;">
 
-      <iframe style="border:none;height:200px;margin-top:10px;overflow:hidden;width:103%;" src="../index_chat.php?chatter=0000000000&amp;chatee=1111111111&amp;topicinit=lobby" id="chatBox"></iframe>
+      <iframe style="border:none;min-height:400px;margin-top:10px;overflow:hidden;width:103%;" src="../index_chat.php?chatter=0000000000&amp;chatee=1111111111&amp;topicinit=lobby" id="chatBox"></iframe>
 
     </div>
 
